c9a3e3622bc7414bcd177bcd3e454fde2c8fd93b
André Lashley <andre.lashley@gmail.com>
2022-05-01 20:44:30 -0700

    Add recipe controller

---
+++ Controllers/RecipeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RecipeGist.Models;
+
+namespace RecipeGist.Controllers
+{
+    public class RecipeController : Controller
+    {
+        private readonly IRecipeRepository _recipeRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public RecipeController(IRecipeRepository recipeRepository, ICategoryRepository categoryRepository)
+        {
+            _recipeRepository = recipeRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public ViewResult List()
+        {
+            return View(_recipeRepository.AllRecipes);
+        }
+    }
+}
