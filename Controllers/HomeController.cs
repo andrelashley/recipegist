1c82fd160b80a1bf91e653cc420c4fb279c7f425
André Lashley <andre.lashley@gmail.com>
2022-05-04 19:02:24 -0700

    Add Home page

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RecipeGist.Models;
+using RecipeGist.ViewModels;
+
+namespace RecipeGist.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IRecipeRepository _recipeRepository;
+
+        public HomeController(IRecipeRepository recipeRepository)
+        {
+            _recipeRepository = recipeRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var homeViewModel = new HomeViewModel
+            {
+                RecipesOfTheWeek = _recipeRepository.RecipesOfTheWeek
+            };
+
+            return View(homeViewModel);
+        }
+    }
+}
---
+++ ViewModels/HomeViewModel.cs
@@ -0,0 +1,10 @@
+﻿using RecipeGist.Models;
+using System.Collections.Generic;
+
+namespace RecipeGist.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Recipe> RecipesOfTheWeek { get; set; }
+    }
+}
